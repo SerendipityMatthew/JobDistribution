8ec33cec7443ec9a507d5faff8102cece360521d
xuwanjin1991@gmail.com <xuwanjin1991@Gmail.com>
2019-04-07 09:33:04 +0800

    rename the project
    Signed-off-by: xuwanjin1991@gmail.com <xuwanjin1991@Gmail.com>

---
+++ CrawlerJobPorject/Company.py
@@ -0,0 +1,11 @@
+class Company:
+    def __init__(self, name, ceo, artificial_person, address, founded_date, company_scale, website):
+        self.name = name
+        self.ceo = ceo
+        self.artificialPerson = artificial_person
+        self.address = address
+        self.foundedDate = founded_date
+        self.companyScale = company_scale
+        self.website = website
+
+    def
---
+++ CrawlerJobPorject/__init__.py
@@ -0,0 +1,5 @@
+
+URL = "https://www.zhipin.com/?city=101020100"
+
+CITY = "上海"
+City_Code = "101020100"
